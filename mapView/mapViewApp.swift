c5dcf7da30c1112ad26f662bb024b1076e2172c8
ganudoomer <ganuiscool@gmail.com>
2021-12-14 21:36:02 +0530

    Initial Commit

---
+++ mapView/mapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapViewApp.swift
+//  mapView
+//
+//  Created by Sree on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct mapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
